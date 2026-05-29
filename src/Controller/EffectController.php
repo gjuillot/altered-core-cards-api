ef432808bd9283d0ac4dfaf84b1c185aea3048d7
John Quairia <john@toxicity.be>
2026-05-29 10:28:32 +0200

    create effect api

---
+++ src/Controller/EffectController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\AbilityEffectRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class EffectController extends AbstractController
+{
+    public function __construct(
+        private readonly AbilityEffectRepository $repository,
+    ) {}
+
+    #[Route('/api/effects', name: 'api_effects', methods: ['GET'])]
+    public function index(): JsonResponse
+    {
+        $result = [];
+
+        foreach ($this->repository->findBy([], ['alteredId' => 'ASC']) as $effect) {
+            $result[] = [
+                'alteredId'     => $effect->getAlteredId(),
+                'filterExample' => 'effectSlot[1][effect]=' . $effect->getAlteredId(),
+                'isSupport'     => $effect->isSupport(),
+                'translations'  => $effect->getText(),
+            ];
+        }
+
+        return $this->json($result);
+    }
+}
